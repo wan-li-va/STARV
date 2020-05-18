f0c236c4f45d639a161572a9595acbb1d429883d
Alex Shen <as5gd@viginia.edu>
2020-05-18 15:39:31 -0400

    Added blank main/stats panels

---
+++ src/MainPanel.js
@@ -0,0 +1,11 @@
+import React, {Component} from "react";
+
+class MainPanel extends Component {
+    render() {
+        return <div>
+            <p>MainPanel goes here</p>
+        </div>
+    }
+}
+
+export default MainPanel;
---
+++ src/StatsPanel.js
@@ -0,0 +1,11 @@
+import React, {Component} from "react";
+
+class StatsPanel extends Component {
+    render() {
+        return <div>
+            <p>StatsPanel goes here</p>
+        </div>
+    }
+}
+
+export default StatsPanel;
