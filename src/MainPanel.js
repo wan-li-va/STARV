fdd2d8a1771d8d87f6184f9d35ef4ca85922215a
Alex Shen <shen.alexander@gmail.com>
2020-05-19 14:31:36 -0400

    Added quotes panel

--- src/MainPanel.js
+++ src/MainPanel.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
 import Timer from './Timer';
-
+import QuotesPanel from "./QuotesPanel"
 export default class MainPanel extends Component {
     render() {
         return <div className="MainPanel">
             <p>MainPanel goes here</p>
             <Timer saveFast={this.props.saveFast} />
+            <QuotesPanel />
         </div>
     }
 }
