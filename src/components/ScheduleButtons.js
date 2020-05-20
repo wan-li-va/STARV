538320633ede568b54c4a52a03d7269f685a25b3
Wan Li <wl9wgc@virginia.edu>
2020-05-20 16:10:30 -0400

    options panel styling in progress

--- src/components/ScheduleButtons.js
+++ src/components/ScheduleButtons.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import OverlayTrigger from "react-bootstrap/OverlayTrigger";
 import Tooltip from "react-bootstrap/Tooltip";
+import '../styling/ScheduleButtons.css';
 
 export default class ScheduleButtons extends Component {
     constructor(props) {
@@ -43,48 +44,52 @@ export default class ScheduleButtons extends Component {
     };
 
     render() {
-        return <div className="radioButtons">
-            <div className="form-check">
-                <input type="radio" name="Radios" id="Radio1" value={16}
-                    checked={this.state.selectedRadio === "Radio1"} onClick={this.handleOptionChange}
-                    disabled={this.props.isRunning} />
-                <label className="form-check-label">
-                    16:8
-                    </label>
-            </div>
-            <div className="form-check">
-                <input type="radio" name="Radios" id="Radio2" value={18}
-                    checked={this.state.selectedRadio === "Radio2"} onClick={this.handleOptionChange}
-                    disabled={this.props.isRunning} />
-                <label className="form-check-label">
-                    18:6
-                    </label>
-            </div>
+        return (
+            <div>
+                <div className="radioButtons">
+                    <div className="form-check">
+                        <input type="radio" name="Radios" id="Radio1" value={16}
+                            checked={this.state.selectedRadio === "Radio1"} onClick={this.handleOptionChange}
+                            disabled={this.props.isRunning} />
+                        <label className="form-check-label">
+                            16:8
+                        </label>
+                    </div>
 
-            <div id="customSelect">
-                <div className="form-check">
-                    <input type="radio" name="Radios" id="Radio3" value="option3"
-                        checked={this.state.selectedRadio === "Radio3"} onClick={this.handleOptionChange}
-                        disabled={this.props.isRunning} />
-                    <label className="form-check-label">
-                        Custom
+                    <div className="form-check">
+                        <input type="radio" name="Radios" id="Radio2" value={18}
+                            checked={this.state.selectedRadio === "Radio2"} onClick={this.handleOptionChange}
+                            disabled={this.props.isRunning} />
+                        <label className="form-check-label">
+                            18:6
                     </label>
-                    <OverlayTrigger placement="right" overlay={
-                        <Tooltip>
-                            Choose intended fasting hours between 5 and 23, inclusive.
-                    </Tooltip>
-                    }>
-                        <div id="customInput">
-                            <input type="number" id="quantity" name="quantity" min="5" max="23" placeholder={this.props.fastLength}
-                                disabled={this.state.selectedRadio !== "Radio3"} onChange={this.handleCustomTime} />
-                            <br />
+                    </div>
 
+                    <div id="customSelect" className="customSelect">
+                        <div className="form-check">
+                            <input type="radio" name="Radios" id="Radio3" value="option3"
+                                checked={this.state.selectedRadio === "Radio3"} onClick={this.handleOptionChange}
+                                disabled={this.props.isRunning} />
+                            <p className="form-check-label">
+                                Custom
+                                </p>
+                            <OverlayTrigger placement="right" overlay={
+                                <Tooltip>
+                                    Choose intended fasting hours between 5 and 23, inclusive.
+                                    </Tooltip>
+                            }>
+                                <div id="customInput">
+                                    <input type="number" id="quantity" name="quantity" min="5" max="23" placeholder={this.props.fastLength}
+                                        disabled={this.state.selectedRadio !== "Radio3"} onChange={this.handleCustomTime} />
+                                </div>
+                            </OverlayTrigger>
                         </div>
-                    </OverlayTrigger>
+                    </div>
                 </div>
-            </div>
-            <div className="timeSince">Time since last fast: {this.props.durationText}
-            </div>
-        </div >
+                <div className="timeSince">
+                    Time since last fast: {this.props.durationText}
+                </div>
+            </div >
+        )
     }
 }
