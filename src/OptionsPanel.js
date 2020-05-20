f84e66966d1e48595f967e036c8d8aa0a66b114c
Ann Hoang <lph2yr@virginia.edu>
2020-05-20 11:26:25 -0400

    play and stop icons

--- src/OptionsPanel.js
+++ src/OptionsPanel.js
@@ -57,14 +57,8 @@ export default class OptionsPanel extends Component {
 
     render() {
         return <div id="OptionsPanel">
-<<<<<<< HEAD
             <Button onClick={this.handleStartStop} disabled={this.state.buttonDisabled}><BsFillPlayFill />Start</Button>
-            <Button variant="danger" onClick={this.handleStartStop} disabled={!this.props.isRunning}><BsFillStopFill /> Stop</Button>
-            {/* <button onClick={this.handleStart}>hi</button> */}
-=======
-            <Button onClick={this.handleStartStop} disabled={this.state.buttonDisabled}>Start</Button>
-            <Button variant="danger" onClick={this.handleStartStop} disabled={!this.props.isRunning}>Stop</Button>
->>>>>>> master
+            <Button variant="danger" onClick={this.handleStartStop} disabled={!this.props.isRunning}><BsFillStopFill />Stop</Button>
 
             <div className="form-check">
                 <input type="radio" name="Radios" id="Radio1" value={16}
