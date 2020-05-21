d187902c1a8f537068a87e4533cc0311b9b2a4e3
Dakota Bourne <44448765+dakotabourne373@users.noreply.github.com>
2020-05-21 12:01:17 -0400

    added tooltip for badge!

--- src/components/BadgePanel.js
+++ src/components/BadgePanel.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import img from '../images/medal.png';
-import '../styling/BadgePanel.css'
+import '../styling/BadgePanel.css';
+import OverlayTrigger from "react-bootstrap/OverlayTrigger";
+import Tooltip from "react-bootstrap/Tooltip";
 
 const Badge = () => { return (<img src={img} alt="Gold Medal!!!!" width="100" height="100" />) }
 
@@ -14,9 +16,15 @@ export default class BadgePanel extends Component {
     badgeHelper = () => {
         if (this.props.numOfBadges > 0)
             return (
-                <p>
-                    <Badge /> x{this.props.numOfBadges}
-                </p>
+                <OverlayTrigger placement="left" overlay={
+                    <Tooltip>
+                        This badge is earned for doing 10 consecutive STARVs 
+                    </Tooltip>
+                }>
+                    <p>
+                        <Badge /> x{this.props.numOfBadges}
+                    </p>
+                </OverlayTrigger >
             );
     }
 
